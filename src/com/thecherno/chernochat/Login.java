7300539401f5e79e69aa05edc99903b7da9bc410
gisalito <gisalito@gisalitopc>
2015-06-23 02:06:31 -0300

    Episodio 3: Todos los botones y botón login.

--- src/com/thecherno/chernochat/Login.java
+++ src/com/thecherno/chernochat/Login.java
@@ -8,6 +8,12 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
+import javax.swing.JButton;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Login extends JFrame {
 
@@ -17,6 +23,12 @@ public class Login extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtName;
+	private JTextField txtAddress;
+	private JLabel lblAddress;
+	private JTextField txtPort;
+	private JLabel lblPort;
+	private JLabel lbleg;
+	private JLabel lbleg_1;
 
 	/**
 	 * Launch the application.
@@ -38,6 +50,14 @@ public class Login extends JFrame {
 	 * Create the frame.
 	 */
 	public Login() {
+		
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			
+		}
+	
+		
 		setResizable(false);
 		setTitle("Login");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -49,12 +69,46 @@ public class Login extends JFrame {
 		contentPane.setLayout(null);
 		
 		txtName = new JTextField();
-		txtName.setBounds(64, 32, 166, 20);
+		txtName.setBounds(64, 25, 166, 20);
 		contentPane.add(txtName);
 		txtName.setColumns(10);
 		
 		JLabel lblNewLabel = new JLabel("Name:");
 		lblNewLabel.setBounds(124, 11, 46, 14);
 		contentPane.add(lblNewLabel);
+		
+		txtAddress = new JTextField();
+		txtAddress.setColumns(10);
+		txtAddress.setBounds(64, 88, 166, 20);
+		contentPane.add(txtAddress);
+		
+		lblAddress = new JLabel("IP Address:");
+		lblAddress.setBounds(112, 74, 70, 14);
+		contentPane.add(lblAddress);
+		
+		txtPort = new JTextField();
+		txtPort.setColumns(10);
+		txtPort.setBounds(64, 158, 166, 20);
+		contentPane.add(txtPort);
+		
+		lblPort = new JLabel("Port:");
+		lblPort.setBounds(124, 146, 33, 14);
+		contentPane.add(lblPort);
+		
+		lbleg = new JLabel("(e.g. 8192)");
+		lbleg.setBounds(106, 184, 76, 14);
+		contentPane.add(lbleg);
+		
+		lbleg_1 = new JLabel("(e.g. 192.168.1.102)");
+		lbleg_1.setBounds(87, 106, 119, 14);
+		contentPane.add(lbleg_1);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnNewButton.setBounds(102, 257, 89, 23);
+		contentPane.add(btnNewButton);
 	}
 }
